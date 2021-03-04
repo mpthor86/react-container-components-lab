c43fcc8afe26a0357caaf10e9220afe43a9bd7c8
michael <mpthornton86@gmail.com>
2021-03-04 10:28:57 -0700

    commit tree 7e6bc01781d85833a68c3bc88379ea976192a4c2

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props => {
+    
+    return (
+        <div className='review-list'>
+            {props.reviews.map(rev => 
+            <li className='review'>
+                <strong>Movie: {rev.display_title}</strong><br></br>
+                <u>Critic:</u> {rev.byline}<br></br>
+                <a href={rev.link.url}>Link To Review</a><br></br>
+                <img src={rev.multimedia.src} alt="" />
+                </li>)}
+        </div>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+//const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+//const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+//            + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    state = {reviews: [], searchTerm: ""}
+
+    handleChange = (e) =>{
+        this.setState({searchTerm: e.target.value})
+    }
+
+    fetchMovies =(e, input) =>{
+        e.preventDefault()
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${input}&api-key=CAjAAtXWqLr21pKiaWuoOvTZST6LAFyS`)
+        .then(resp => resp.json())
+        .then(json => this.setState({reviews: json['results']}))
+        
+    }
+    
+    render() {
+        return(
+            <div className="searchable-movie-reviews">
+                Search for Reviews by Movie:
+                <form onSubmit={(e) => this.fetchMovies(e, this.state.searchTerm)}><input type='text' 
+                value={this.state.searchTerm} 
+                onChange={this.handleChange}/></form>
+                <div>        
+                    <MovieReviews reviews={this.state.reviews} />
+                </div>
+            </div>
+        )
+    }
+}
